038db2a35bbb69caed391fd01eb71c01ca68ee00
Roman Nazarenko <me@jtalk.me>
2019-04-24 23:11:32 +0100

    React seems to only allow REACT_APP_ properties to be propagated.

--- digito-ui/src/utils/config.js
+++ digito-ui/src/utils/config.js
@@ -1,6 +1,7 @@
 import * as config from "react-global-configuration";
 
-let API = process.env.API_LOCATION || "http://localhost:5000";
+
+let API = process.env.REACT_APP_API_LOCATION || "http://localhost:5000";
 
 config.set({
     api: API
